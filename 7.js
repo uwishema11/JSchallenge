99cfed42c88ff4708263d778a4353bac40ce094a
uwishema11 <99412054+uwishema11@users.noreply.github.com>
2022-03-30 19:19:36 +0200

    exercise7

--- 7.js
+++ 7.js
@@ -1,10 +1,12 @@
-const array = [1,2,3];
-const newArray = [];
-let reverseArray =(array) => {
-    for(let a= array.length-1; a >=0 ;a--){
-        newArray.push(array[a]);
-    }
-    return newArray;
-};
+// a Javascript function that takes an array of numbers and return a reversed version. 
+// Note: here you are not allowed to use an intermediary array. You need to use the same array passed in
+//  the parameter and just reverse its content. Do not use inbuilt functions like reverse(). Just use loops and conditions.
 
-console.log(reverseArray(array));
+function isReverse(array){
+    let output = [];
+    for(let i=array.length - 1; i>=0;i--){
+      output.push(array[i]);
+    }
+  return output;
+}
+console.log(isReverse([22,7,8,9,5]));
