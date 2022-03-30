3b1c2d60b08886bdff5c4295574e4132be51235e
uwishema11 <99412054+uwishema11@users.noreply.github.com>
2022-03-30 18:42:18 +0200

    exercise 1 and exercise 2

--- 1.js
+++ 1.js
@@ -1,12 +1,24 @@
-var oldArray=[2,4,5,7,8,31,11];
-var theArray=[];
-
-function primeNumber(array){
-for( var i=0;i<array.length; i++){
-    if(array[i]%2 !=0){
-        theArray.push(array[i])
+function primeNumbers(array){
+    let primeArray = [];
+    for(var i = 0; i < array.length; i++) {
+        let isPrime = true;
+        if(array[i] < 2 && array[i] >=0) {
+            isPrime = false;
+        }else if(array[i] == 2) {
+            isPrime = true;
+        }else{
+            for(var j = 2; j < array[i]; j++) {
+                if(array[i] % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if(isPrime == true) {
+            primeArray.push(array[i]);
+        }
     }
+    return primeArray;
 }
-return theArray;
-}
-console.log(primeNumber(oldArray));
+
+console.log(primeNumbers([1,2,5,3,22]))
--- 8.js
+++ 8.js
@@ -1,9 +1,24 @@
-const array = ["Patrick wyne, 30", "lil wyne, 32","Eric mimi, 21","Dodos deck, 21","Alian Dwine, 22","Patrick wyne, 33","Patrick wyne, 100","Patrick wyne, 40"];
-const nestedObject = {};
-
-let objectFunction =(array) => { 
-    array.map(el =>nestedObject[el.split(" ")[0]]= { second_name: el.split(" ")[1].slice(0,-1), age: el.split(" ")[2]} )
-    return nestedObject;
-};
-
-console.log(objectFunction(array));
+// a function that takes a formatted array. The array is made of a string of people’s identities in a predefined format, 
+// [“Patrick wyne, 30”, “lil wyne, 32”,“Eric mimi, 21”,“Dodos deck, 21”,“Alian Dwine, 22”,“Patrick wyne, 33”,“Patrick wyne, 100”,“Patrick wyne, 40”]
+// {
+//     patrick: {second-name:wyne, age:30},
+//  lil: {second-name:wyne, age:30},
+//  ...
+// }
+function getPeople(array) {
+    let output = {};
+    for(let i = 0; i < array.length; i++) {
+        let firstName = array[i].split(" ")[0];
+        let lastName = array[i].split(" ")[1].split(",")[0];
+        let age = array[i].split(" ")[2];
+        let myObj = {
+            second_name: lastName,
+            age: parseInt(age)
+        }
+        output[firstName] = myObj;
+        
+    }
+    return output;
+}
+let peopleObj = getPeople(["Patrick wyne, 30", "lil wyne, 32","Eric mimi, 21","Dodos deck, 21"]);
+console.log(peopleObj)
