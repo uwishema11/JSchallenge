da5fb3971c5e98ea092c4d3bea9f421cb4c8c016
uwishema11 <99412054+uwishema11@users.noreply.github.com>
2022-03-30 17:17:40 +0200

    exercise 2

--- 2.js
+++ 2.js
@@ -1,4 +1,12 @@
-const string = "radar";
-let palindromeChecker =(string) =>  string.split('').reverse().join('') === string;
-
-console.log(palindromeChecker(string));
+//simple js  function that return true/false wether a Palindrome or not
+ 
+function isPalindrome(name){
+    let string = name.split("").reverse().join("");
+    if (string == name){
+       return true;
+    }else{
+       return false;
+    }
+}
+   
+console.log(isPalindrome("2002"));
